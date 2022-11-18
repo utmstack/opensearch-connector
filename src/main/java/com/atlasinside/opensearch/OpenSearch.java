ec2b9d94efcf8c2a69b735aa2d0e7098ecb6d2a7
Leonardo Mora López <41559395+leonardomoralopez89@users.noreply.github.com>
2022-11-18 14:28:37 +0200

    Update OpenSearch.java
    Implementing search opearation

--- src/main/java/com/atlasinside/opensearch/OpenSearch.java
+++ src/main/java/com/atlasinside/opensearch/OpenSearch.java
@@ -9,6 +9,9 @@ import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.opensearch.client.RestClient;
 import org.opensearch.client.json.jackson.JacksonJsonpMapper;
 import org.opensearch.client.opensearch.OpenSearchClient;
+import org.opensearch.client.opensearch._types.query_dsl.Query;
+import org.opensearch.client.opensearch.core.SearchRequest;
+import org.opensearch.client.opensearch.core.SearchResponse;
 import org.opensearch.client.transport.OpenSearchTransport;
 import org.opensearch.client.transport.rest_client.RestClientTransport;
 
@@ -17,7 +20,26 @@ import java.util.List;
 import java.util.Objects;
 
 public class OpenSearch {
+    private static final String CLASSNAME = "OpenSearch";
     private final OpenSearchClient client;
+
+    /**
+     * Perform a search operation
+     *
+     * @param query        Query to executed
+     * @param index        Index were the search will be performed, you can use a pattern too
+     * @param responseType Type of the object that will be mapped in the response
+     * @return A {@link SearchResponse} object with the results of the performed search
+     */
+    public <T> SearchResponse<T> search(Query query, String index, Class<T> responseType) {
+        final String ctx = CLASSNAME + ".search";
+        try {
+            return client.search(new SearchRequest.Builder().index(index)
+                    .query(query).build(), responseType);
+        } catch (Exception e) {
+            throw new RuntimeException(ctx + ": " + e.getLocalizedMessage());
+        }
+    }
 
     private OpenSearch(OpenSearchClient client) {
         this.client = client;
@@ -54,7 +76,6 @@ public class OpenSearch {
                 if (hosts.isEmpty())
                     throw new RuntimeException("No hosts definition were provided");
 
-                //Initializing the client
                 RestClient restClient = RestClient.builder(hosts.toArray(new HttpHost[0])).
                         setHttpClientConfigCallback(builder -> builder.setDefaultCredentialsProvider(credentialsProvider))
                         .build();
